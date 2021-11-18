58a94a22fee5fec0d16cc2d04701efb2f99fb74b
Viachaslau Kulchynski <v.kulchynski@nord-soft.com>
2021-11-18 15:57:00 +0300

    connect database

--- api.py
+++ api.py
@@ -2,37 +2,21 @@ import shutil
 from typing import List
 from fastapi import APIRouter, UploadFile, File, Form
 from schemas import UploadVideo, GetVideo
+from models import Video, User
 
 video_router = APIRouter()
 
 
 @video_router.post("/")
-async def root(title: str = Form(...), description: str = Form(...), file: UploadFile = File(...)):
+async def create_video(title: str = Form(...), description: str = Form(...), file: UploadFile = File(...)):
     info = UploadVideo(title=title, description=description)
     with open(f'{file.filename}', "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
-
-    return {"file_name": file.filename, "info": info}
-
-
-@video_router.post("/img")
-async def upload_img(files: List[UploadFile] = File(...)):
-    for img in files:
-        with open(f'{img.filename}', "wb") as buffer:
-            shutil.copyfileobj(img.file, buffer)
-
-    return {"file_name": "It's oK"}
+    user = await User.objects.first()
+    return await Video.objects.create(file=file.filename, user=user, **info.dict())
 
 
-@video_router.post("/info")
-async def info_set(info: UploadVideo):
-    return info
-
-
-@video_router.get("/video", response_model=GetVideo)
-async def get_video():
-    user = {'id': 12, 'name': 'Arni'}
-    video = {'title': 'Test', 'description': 'Description'}
-    return GetVideo(user=user, video=video)
-
-
+@video_router.get("/video/{video_pk}")
+async def get_video(video_pk: int):
+    video = await Video.objects.select_related("user").get(pk=video_pk)
+    return video
